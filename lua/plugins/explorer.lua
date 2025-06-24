712c1d771ae9c45333663ce3129d1df2564d85da
shivkumarojha <shivkmojha@gmail.com>
2025-06-24 22:21:06 +0530

    added image support

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -1,52 +1,51 @@
 return {
-	"folke/snacks.nvim",
-	priority = 1000,
-	lazy = false,
-	opts = {
-		picker = {
-			enabled = true,
-			exclude = { -- add folder names here to exclude
-				".git",
-				"node_modules",
-			},
-			sources = {
-				explorer = {
-					auto_close = true,
-					hidden = true,
-					layout = {
-						finder = "explorer",
-						layout = {
-							preset = "sidebar",
-							preview = false,
-							position = "right",
-							width = 25,
-						},
-						cycle = false,
-					},
-				},
-			},
-			win = {
-				input = {
-					keys = {
-						["<a-h>"] = false, -- unbind <a-h>
-					},
-				},
-				list = {
-					keys = {
-						["<a-h>"] = false, -- unbind <a-h> here too
-					},
-				},
-			},
-		},
-	},
-
-	keys = {
-		{
-			"<leader>e",
-			function()
-				Snacks.explorer.open()
-			end,
-			desc = "Open Snacks Picker",
-		},
-	},
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    picker = {
+      enabled = true,
+      exclude = { -- add folder names here to exclude
+        ".git",
+        "node_modules",
+      },
+      sources = {
+        explorer = {
+          auto_close = true,
+          hidden = true,
+          layout = {
+            finder = "explorer",
+            layout = {
+              preset = "sidebar",
+              preview = false,
+              position = "right",
+              width = 25,
+            },
+            cycle = false,
+          },
+        },
+      },
+      win = {
+        input = {
+          keys = {
+            ["<a-h>"] = false, -- unbind <a-h>
+          },
+        },
+        list = {
+          keys = {
+            ["<a-h>"] = false, -- unbind <a-h> here too
+          },
+        },
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader>e",
+      function()
+        Snacks.explorer.open()
+      end,
+      desc = "Open Snacks Picker",
+    },
+  },
 }
