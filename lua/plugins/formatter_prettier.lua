ded00c0a48c44e76e7ebbbb334988a9f5111b0e7
shivkumarojha <shivkmojha@gmail.com>
2025-06-25 11:02:29 +0530

    added default formatter to prettier for lazyvim

---
+++ lua/plugins/formatter_prettier.lua
@@ -0,0 +1,16 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      javascript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescript = { "prettier" },
+      typescriptreact = { "prettier" },
+      json = { "prettier" },
+      html = { "prettier" },
+      css = { "prettier" },
+      markdown = { "prettier" },
+      yaml = { "prettier" },
+    },
+  },
+}
