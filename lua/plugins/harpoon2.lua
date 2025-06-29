865464ae0e021399ac3a55f7a990752477910634
shivkumarojha <shivkmojha@gmail.com>
2025-06-29 14:56:57 +0530

    final harppon working yay yay

--- lua/plugins/harpoon2.lua
+++ lua/plugins/harpoon2.lua
@@ -9,49 +9,49 @@ return {
       save_on_toggle = true,
     },
   },
-keys = function()
-  local keys = {
-    {
-      "<leader>H",
-      function()
-        require("harpoon"):list():add()
-      end,
-      desc = "Harpoon File",
-    },
-    {
-      "<leader>h",
-      function()
-        local harpoon = require("harpoon")
+  keys = function()
+    local keys = {
+      {
+        "<leader>H",
+        function()
+          require("harpoon"):list():add()
+        end,
+        desc = "Harpoon File",
+      },
+      {
+        "<leader>h",
+        function()
+          local harpoon = require("harpoon")
           harpoon.ui:toggle_quick_menu(harpoon:list())
-      end,
-      desc = "Harpoon Quick Menu",
-    },
-    {
-      "<C-h>",
-      function()
-        require("harpoon"):list():prev()
-      end,
-      desc = "Harpoon Prev File",
-    },
-    {
-      "<C-l>",
-      function()
-        require("harpoon"):list():next()
-      end,
-      desc = "Harpoon Next File",
-    },
-  }
+        end,
+        desc = "Harpoon Quick Menu",
+      },
+      {
+        "<C-h>",
+        function()
+          require("harpoon"):list():prev()
+        end,
+        desc = "Harpoon Prev File",
+      },
+      {
+        "<C-l>",
+        function()
+          require("harpoon"):list():next()
+        end,
+        desc = "Harpoon Next File",
+      },
+    }
 
-  for i = 1, 5 do
-    table.insert(keys, {
-      "<leader>" .. i,
-      function()
-        require("harpoon"):list():select(i)
-      end,
-      desc = "Harpoon to File " .. i,
-    })
-  end
+    for i = 1, 5 do
+      table.insert(keys, {
+        "<leader>" .. i,
+        function()
+          require("harpoon"):list():select(i)
+        end,
+        desc = "Harpoon to File " .. i,
+      })
+    end
 
-  return keys
-end,
+    return keys
+  end,
 }
