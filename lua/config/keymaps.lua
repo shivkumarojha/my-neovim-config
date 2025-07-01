f35191eceb286f1540bf18b11b0e4174062b04ba
shivkumarojha <shivkmojha@gmail.com>
2025-07-01 11:25:29 +0530

    added jj keybinding for escape

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.keymap.set("i", "jj", "<ESC>", { silent = true })
 vim.keymap.set("n", "<CapsLock>", "<Esc>", { noremap = true, silent = true })
 vim.keymap.set("i", "<CapsLock>", "<Esc>", { noremap = true, silent = true })
 vim.keymap.set("v", "<CapsLock>", "<Esc>", { noremap = true, silent = true })
