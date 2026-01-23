6ba177e0645bd032455b31957a57af5b948919b9
shivkumarojha <shivkmojha@gmail.com>
2026-01-23 14:57:14 +0530

    some minor changes

---
+++ lua/plugins/tinted_color.lua
@@ -0,0 +1,8 @@
+return {
+  "tinted-theming/tinted-nvim",
+  lazy = false, -- Make sure the colorscheme loads early
+  priority = 1000,
+  config = function()
+    -- Optional: set global options here
+  end,
+}
