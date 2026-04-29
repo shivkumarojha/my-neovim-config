a990da33e4d9871b3f406f2822062fe3fd1663cc
shivkumarojha <shivkmojha@gmail.com>
2026-04-30 01:55:39 +0530

    fix: critical enter behaviour changes after visual multi on and off. fixed

--- lua/plugins/visual-multi.lua
+++ lua/plugins/visual-multi.lua
@@ -2,4 +2,9 @@ return {
   "mg979/vim-visual-multi",
   branch = "master",
   lazy = false, -- Ensure the plugin loads immediately
+  config = function()
+    vim.g.VM_maps = vim.tbl_extend("force", vim.g.VM_maps or {}, {
+      ["I Return"] = "",
+    })
+  end,
 }
